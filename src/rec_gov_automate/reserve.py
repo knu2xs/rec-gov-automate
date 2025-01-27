fd081b879b6c94a27e527f6110d31ec4904a66d8
Joel McCune <knu2xs@gmail.com>
2025-01-27 13:40:11 -0800

    middle fork and main

--- src/rec_gov_automate/reserve.py
+++ src/rec_gov_automate/reserve.py
@@ -1,10 +1,192 @@
 from datetime import datetime, timedelta
 import re
-from typing import Optional
+from typing import Optional, Union
 
 from playwright.sync_api import sync_playwright
 
 __all__ = ["reserve_middle_fork_permit_date"]
+
+
+def reserve_4rivers_permit_date(
+    permit_id: Union[str, int],
+    recgov_username: str,
+    recgov_password: str,
+    day: int,
+    month: int,
+    launch_location_code: Union[str, int],
+    takeout_location_code: Union[str, int],
+    pickup_permit_location_code: Union[str, int],
+    trip_days: Optional[int],
+    year: Optional[int] = None,
+    headless: Optional[bool] = True,
+) -> bool:
+    """
+    Reserve four rivers river permit for a specific date.
+
+    Args:
+        permit_id: Unique permit ID on Recreation.gov.
+        recgov_username: Username for Recreation.gov.
+        recgov_password: Password for Recreation.gov.
+        day: Day of reservation.
+        month: Month of reservation.
+        launch_location_code: Launch location code...has to be derived from inspecting the reservation page.
+        takeout_location_code: Takeout location code...has to be derived from inspecting the reservation page.
+        pickup_permit_location_code: Code for location where permit will be picked up location code...has to be
+          derived from inspecting the reservation page.
+        year: Year of reservation. Defaults to current year.
+        trip_days: Trip duration in days. Defaults to 7.
+        headless: Whether to use the browser headless mode. Default is True.
+
+    Returns:
+
+    """
+    # default to current year
+    if year is None:
+        year = datetime.now().year
+
+    # create a datetime for the day being retrieved
+    dt = datetime(year, month, day)
+
+    # get the sunday datetime for the date being retrieved
+    dt_sun = dt - timedelta(days=dt.weekday() + 1)
+
+    # create the url to retrieve
+    url = (
+        f"https://www.recreation.gov/permits/{permit_id}/registration/detailed-availability"
+        f"?date={dt_sun.year:04d}-{dt_sun.month:02d}-{dt_sun.day:02d}"
+    )
+
+    # create a playwright page to use
+    with sync_playwright() as pw:
+        browser = pw.chromium.launch(headless=headless)
+        page = browser.new_page()
+        page.goto(url)
+
+        # try to get the date being searched for
+        avail_re = rf"""((\b{dt.strftime("%B")}\b)|(\b{dt.strftime("%b")}\b))\s{dt.day:02d},\s{dt.year:04d}\s-\sAvailable"""
+        loc = page.get_by_label(re.compile(avail_re))
+
+        # gives the page a moment to load content
+        loc.hover()
+
+        # if the date is not available, set status to return
+        if loc.count() == 0:
+            status = False
+
+        # if the date is available
+        else:
+
+            # select the date
+            loc.click()
+
+            # click the book now button
+            page.get_by_role("button", name="Book Now").click()
+
+            # modal div will be presented to log in
+
+            # populate the information and log in
+            page.get_by_label("Email* (Required)").fill(recgov_username)
+            page.get_by_label("Password* (Required)").fill(recgov_password)
+            page.get_by_role("button", name="Log In", exact=True).click()
+
+            # permit will be put in cart, and form page will be loaded to input details for permit
+
+            # launch at boundary creek
+            if not headless:
+                page.get_by_label(
+                    "Launch Location* (Required)"
+                ).scroll_into_view_if_needed()
+
+            page.get_by_label("Launch Location* (Required)").select_option(
+                str(launch_location_code)
+            )
+
+            # take-out at cache bar boat ramp
+            if not headless:
+                page.get_by_label(
+                    "Take-out Location* (Required)"
+                ).scroll_into_view_if_needed()
+
+            page.get_by_label("Take-out Location* (Required)").select_option(
+                str(takeout_location_code)
+            )
+
+            # open calendar selector
+            if not headless:
+                page.get_by_role(
+                    "button", name="Exit Date (Required)"
+                ).scroll_into_view_if_needed()
+            else:
+                page.get_by_role("button", name="Exit Date (Required)").hover()
+
+            page.get_by_role("button", name="Exit Date (Required)").click()
+
+            # create string to select date by the div label
+            dt_exit = dt + timedelta(days=trip_days - 1)
+            # dt_exit_str = dt_exit.strftime(r"%A, %b %-d, %Y - Available")
+            dt_exit_re = rf"""((\b{dt_exit.strftime("%B")}\b)|(\b{dt_exit.strftime("%b")}\b))\s{dt_exit.day:02d},\s{dt_exit.year:04d}\s-\sAvailable"""
+
+            # select and click the exit date, which closes the calendar selector
+            if not headless:
+                page.get_by_label(re.compile(dt_exit_re)).scroll_into_view_if_needed()
+            else:
+                page.get_by_label(re.compile(dt_exit_re)).hover()
+
+            page.get_by_label(re.compile(dt_exit_re)).click()
+
+            # select one craft, a single raft
+            if not headless:
+                page.get_by_label("Type* (Required)").scroll_into_view_if_needed()
+
+            page.get_by_label("Type* (Required)").select_option("10")
+
+            # click to add single watercraft
+            if not headless:
+                page.get_by_label("Type* (Required)").scroll_into_view_if_needed()
+
+            page.get_by_label("Add watercraft").click()
+
+            # pick up permit at boundary creek (issuing station)
+            if not headless:
+                page.get_by_label(
+                    "Station Location* (Required)"
+                ).scroll_into_view_if_needed()
+
+            page.get_by_label("Station Location* (Required)").select_option(
+                str(pickup_permit_location_code)
+            )
+
+            # click terms block
+            if not headless:
+                page.get_by_label(
+                    "Need to Know", exact=True
+                ).scroll_into_view_if_needed()
+
+            page.get_by_label("Need to Know", exact=True).click()
+
+            # scroll all terms
+            page.mouse.wheel(delta_x=0, delta_y=700)
+
+            # click acceptance of terms
+            if not headless:
+                page.locator("label").filter(
+                    has_text="Yes, I have read and agree to"
+                ).locator("span").first.scroll_into_view_if_needed()
+
+            page.locator("label").filter(
+                has_text="Yes, I have read and agree to"
+            ).locator("span").first.check()
+
+            # click view cart to commit trip properties (speeds up checkout later)
+            page.get_by_test_id("OrderDetailsSummary-cart-btn").click()
+
+            # set status to return
+            status = True
+
+        # close the browser
+        browser.close()
+
+        return status
 
 
 def reserve_middle_fork_permit_date(
--- testing/test_rec_gov_automate.py
+++ testing/test_rec_gov_automate.py
@@ -1,5 +1,105 @@
-from rec_gov_automate.reserve import remove_reservations
+from rec_gov_automate.availability import get_river_permit_availability_by_month
+from rec_gov_automate.reserve import reserve_4rivers_permit_date, remove_reservations
+from rec_gov_automate.utils import get_recgov_credentials
+
+headless = False
 
 
 def test_remove_reservations(recgov_credentials):
     remove_reservations(*recgov_credentials, headless=False)
+
+
+def test_reserve_river_permit_date_middle_fork(secrets_path):
+
+    # unique middle fork properties
+    permit_id = 234623  # middle fork
+    month = 12  # December
+    day = 12
+    putin_id = 2100  # boundary creek
+    takeout_id = 2108  # main
+    pickup_permit_loc_id = 2300  # boundary creek
+    days = 7
+
+    # default to not passing
+    reserved = False
+
+    # get available dates
+    avail_df = get_river_permit_availability_by_month(permit_id, start_month=month)
+
+    # filer to just the availability on the day of the month and available permits
+    avail_df = avail_df.loc[
+        (avail_df["date"].dt.day == day) & (avail_df["remaining"] > 0)
+    ]
+
+    # create boolean indicating if there is availability
+    avail = avail_df.shape[0] == 1
+
+    # read the secrets file and get credentials
+    recgov_username, recgov_password = get_recgov_credentials(secrets_path)
+
+    # if there is availability on the requested date
+    if avail:
+
+        # make the reservation
+        reserved = reserve_4rivers_permit_date(
+            permit_id,
+            recgov_username,
+            recgov_password,
+            day,
+            month,
+            putin_id,
+            takeout_id,
+            pickup_permit_loc_id,
+            days,
+            headless=headless,
+        )
+
+    assert reserved
+
+
+def test_reserve_river_permit_date_main_salmon(secrets_path):
+
+    # unique middle fork properties
+    permit_id = 234622  # main salmon
+    month = 12  # December
+    day = 12
+    putin_id = 2400  # corn creek
+    takeout_id = 2409  # carey creek
+    pickup_permit_loc_id = 2412  # corn creek
+    days = 7
+
+    # default to not passing
+    reserved = False
+
+    # get available dates
+    avail_df = get_river_permit_availability_by_month(permit_id, start_month=month)
+
+    # filer to just the availability on the day of the month and available permits
+    avail_df = avail_df.loc[
+        (avail_df["date"].dt.day == day) & (avail_df["remaining"] > 0)
+    ]
+
+    # create boolean indicating if there is availability
+    avail = avail_df.shape[0] == 1
+
+    # read the secrets file and get credentials
+    recgov_username, recgov_password = get_recgov_credentials(secrets_path)
+
+    # if there is availability on the requested date
+    if avail:
+
+        # make the reservation
+        reserved = reserve_4rivers_permit_date(
+            permit_id,
+            recgov_username,
+            recgov_password,
+            day,
+            month,
+            putin_id,
+            takeout_id,
+            pickup_permit_loc_id,
+            days,
+            headless=headless,
+        )
+
+    assert reserved
