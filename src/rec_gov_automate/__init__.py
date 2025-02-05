2dbf8d09d3695c043c98a4cf95b90be57adaf3f6
Joel McCune <knu2xs@gmail.com>
2025-02-05 00:11:00 -0800

    messaging working

--- src/rec_gov_automate/__init__.py
+++ src/rec_gov_automate/__init__.py
@@ -4,8 +4,9 @@ __author__ = "Joel McCune (https://joelmccune.com)"
 __license__ = "Apache 2.0"
 __copyright__ = "Copyright 2023 by Joel McCune (https://joelmccune.com)"
 
-__all__ = ["availability", "reserve", "utils"]
+__all__ = ["availability", "reserve", "send_message", "utils"]
 
 from . import availability
 from . import reserve
 from . import utils
+from .notification import send_message
